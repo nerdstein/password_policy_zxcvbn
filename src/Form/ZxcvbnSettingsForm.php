a9c2612bbc5869ca88b29a5e1c362942461a8bef
Adam Bergstein <adam.bergstein@acquia.com>
2014-11-16 21:07:50 -0500

    Adding schema and settings form

---
+++ src/Form/ZxcvbnSettingsForm.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Drupal\password_policy_zxcvbn\Form;
+
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+
+class PasswordPolicyLengthSettingsForm extends FormBase {
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getFormId() {
+		return 'zxcvbn_settings_form';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function buildForm(array $form, FormStateInterface $form_state) {
+		$form = array();
+
+		//get policy
+		$policy_id = '';
+		$path_args = explode('/', current_path());
+		if(count($path_args)==7) {
+			$policy_id = $path_args[6];
+			//load the policy
+			$policy = db_select('password_policy_zxcvbn_policies', 'p')->fields('p')->condition('pid', $policy_id)->execute()->fetchObject();
+		}
+
+		$form['pid'] = array(
+			'#type' => 'hidden',
+			'#value' => (is_numeric($policy_id))?$policy_id:'',
+		);
+
+		$form['score'] = array(
+			'#type' => 'textfield',
+			'#title' => t('Minimum Zxcvbn Score'),
+			'#default_value' => (is_numeric($policy_id))?$policy->score:'',
+		);
+
+		$form['submit'] = array(
+			'#type'=>'submit',
+			'#value'=>'Add Policy',
+		);
+
+		return $form;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function validateForm(array &$form, FormStateInterface $form_state) {
+		//TODO - Why is this not loading in the admin config page?
+		if(!is_numeric($form_state->getValue('score')) or $form_state->getValue('score')<0) {
+			$form_state->setErrorByName('score', $this->t('The score must be a positive number.'));
+		}
+		//TODO - Add validation for unique number
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function submitForm(array &$form, FormStateInterface $form_state) {
+		if($form_state->getValue('pid')) {
+			db_update('password_policy_zxcvbn_policies')
+				->fields(array('score' => $form_state->getValue('score')))
+				->condition('pid', $form_state->getValue('pid'))
+				->execute();
+		} else {
+			db_insert('password_policy_zxcvbn_policies')
+				->fields(array('score'))
+				->values(array('score' => $form_state->getValue('score')))
+				->execute();
+		}
+		drupal_set_message('Zxcvbn policy settings have been stored');
+	}
+}
---
+++ src/Plugin/PasswordConstraint/Zxcvbn.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\password_policy_zxcvbn\Constraints\Zxcvbn.
+ */
+
+//TODO - Add in "tokens" into annotations (see: error message, which should show #chars from config)
+
+namespace Drupal\password_policy_zxcvbn\Plugin\PasswordConstraint;
+
+use Drupal\password_policy\PasswordConstraintBase;
+use Drupal\Core\Config\Config;
+
+/**
+ * Enforces a specific character length for passwords.
+ *
+ * @PasswordConstraint(
+ *   id = "password_policy_zxcvbn_constraint",
+ *   title = @Translation("Zxcvbn"),
+ *   description = @Translation("Zxcvbn-PHP is a password strength estimator using pattern matching and minimum entropy calculation"),
+ *   error_message = @Translation("Your password lacks strength and has too many common patterns."),
+ *   policy_path = "admin/config/security/password/constraint/zxcvbn",
+ *   policy_update_path = "admin/config/security/password/constraint/zxcvbn/@pid",
+ *   policy_update_token = "@pid"
+ * )
+ */
+class Zxcvbn extends PasswordConstraintBase {
+
+	/**
+	 * Returns a true/false status as to if the password meets the requirements of the constraint.
+	 * @param password
+	 *   The password entered by the end user
+	 * @return boolean
+	 *   Whether or not the password meets the constraint in the plugin.
+	 */
+	function validate($policy_id, $password) {
+		//TODO - get user data from form
+		$userData = array(
+			'Marco',
+			'marco@example.com'
+		);
+
+		$zxcvbn = new \Drupal\password_policy_zxcvbn\Zxcvbn();
+		$strength = $zxcvbn->passwordStrength('password', $userData);
+
+		//TODO - check this against the policy
+		echo $strength['score'];
+	}
+
+	/**
+	 * Returns an array of key value pairs, key is the ID, value is the policy.
+	 *
+	 * @return array
+	 *   List of policies.
+	 */
+	function getPolicies() {
+		$policy = db_select('password_policy_length_policies', 'p')
+			->fields('p');
+
+		$policies = $policy->execute()->fetchAll();
+		$array = array();
+		foreach($policies as $policy){
+			$array[$policy->pid] = 'Minimum character length ' . $policy->character_length;
+		}
+		return $array;
+	}
+}
